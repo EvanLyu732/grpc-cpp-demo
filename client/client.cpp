1201163ec2b9e65b7dee3c9b9eb15cd994c2ea9a
EvanLyu732 <zheng.lv@moying.ai>
2022-04-05 16:18:43 +0800

    update file

--- client/client.cpp
+++ client/client.cpp
@@ -6,8 +6,8 @@
 #include "../proto_generation/product.grpc.pb.h"
 #include <grpc++/grpc++.h>
 #include <fmt/core.h>
-#include <map>
 #include <random>
+#include <chrono>
 #include <utility>
 #include <csignal>
 #include <thread>
@@ -16,9 +16,8 @@ using ecommerce::Product;
 using ecommerce::ProductId;
 using ecommerce::ProductInfo;
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
+using grpc::ClientReader;
+using grpc::ClientContext;
 using grpc::Status;
 
 class Client{
@@ -41,11 +40,26 @@ public:
 
     //TODO
     bool addProduct(){
-        return true;
+        std::shared_ptr<Product> product = this->generateRandomProduct();
+        const Product request = std::as_const(*product);
+        ClientContext context;
+        context.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds(5));
+        std::cout << "Product id: " << request.id() << '\n';
+        ProductId* response{};
+        Status status = _stub->addProduct(&context, request, response);
+        std::cout << "status code is: " << status.error_code() << '\n';
+        if (status.ok()){
+            std::cout << "add product successed, product id:" << request.id() << '\n';
+            return true;
+        } else {
+            std::cout << "add product failed" << '\n';
+            return false;
+        }
     }
 
 
     //TODO
+    [[deprecated()]]
     bool getProduct(){
         return true;
     }
@@ -57,16 +71,21 @@ private:
 
 void signalHandler(int sig) {
     std::cout << "Client stop running!" << '\n';
+    std::terminate();
 }
 
 auto initialSignalHandler() {
-    signal(SIGTERM, signalHandler);
+    signal(SIGINT, signalHandler);
 }
 
 
-auto main() -> int
-{
-    Client client{grpc::CreateChannel("127.0.0.1:5001", grpc::InsecureChannelCredentials())};
-
-
+auto main() -> int {
+    Client client{grpc::CreateChannel("127.0.0.1:50051", grpc::InsecureChannelCredentials())};
+    std::thread([&]() {
+        while (1) {
+            client.addProduct();
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    }).join();
+    return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -22,12 +22,13 @@ using grpc::Status;
 class ServerServiceImpl final: public ProductInfo::Service{
 public:
     Status addProduct(ServerContext* context, const Product* request, ProductId* response){
-        fmt::print("server receives add product call...");
+        std::cout<< "server receives add product call..." << '\n';
         ProductId id;
-        id.set_value(this->productId);
+        id.set_value(std::stoi(request->id()));
         Product* product{std::move(const_cast<Product*>(request))};
-        std::cout << "product map ready to emplace back product, product id:" << this->productId << '\n';
+        std::cout << "product map ready to emplace back product, product id:" << request->id() << '\n';
         this->productMap.insert(std::make_pair(&id, product));
+        std::cout << "add Product service successed" << '\n';
         return Status::OK;
     }
 
@@ -51,10 +52,11 @@ private:
 
 void signalHandler(int sig) {
     std::cout << "Server stop running!" << '\n';
+    std::terminate();
 }
 
 auto initialSignalHandler() {
-    signal(SIGTERM, signalHandler);
+    signal(SIGINT, signalHandler);
 }
 
 
